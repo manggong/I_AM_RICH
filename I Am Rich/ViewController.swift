52c1c28a680e158b1f6e371d21983a0f50776fc3
manggong <jihwank1995@gmail.com>
2020-10-18 19:35:39 +0900

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 김지환 on 2020/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
